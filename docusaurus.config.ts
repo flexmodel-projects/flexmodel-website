ed851d9d752a53f6d8f7be38d615d277affa04d5
Cheng Jinbao <cjbi@outlook.com>
2024-11-07 10:14:34 +0800

    update docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-              'https://github.com/flexmodel-projects/flexmodel-docs/edit/main',
+              'https://github.com/flexmodel-projects/flexmodel-website/edit/main',
         },
         blog: {
           showReadingTime: true,
